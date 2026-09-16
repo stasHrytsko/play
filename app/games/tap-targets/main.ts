bc7b8bbbd42528a58c5a6bffdb864a53843ecc7e
stasHrytsko <65148666+stasHrytsko@users.noreply.github.com>
2026-09-16 18:42:22 +0200

    Apply shared UI kit styles to tap-targets

--- app/games/tap-targets/main.ts
+++ app/games/tap-targets/main.ts
@@ -1,5 +1,7 @@
+import '../../src/styles/ui-kit.css';
 import { bootShell } from '../../src/shell/boot.ts';
 import { GAME } from './game.config.ts';
 import { createMechanicHost } from './mechanic/index.ts';
+import './mechanic/tap-targets.css';
 
 void bootShell(GAME, createMechanicHost());
